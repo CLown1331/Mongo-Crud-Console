bd7962123fa32dd02e1af9739061ebb77160b7dd
CLown1331 <clown1331@gmail.com>
2019-11-17 19:54:23 +0600

    init

---
+++ Mongo-Crud-Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mongo_Crud_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
