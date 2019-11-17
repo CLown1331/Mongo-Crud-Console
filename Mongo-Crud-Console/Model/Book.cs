bb86892d2500f6b8e9fbdab70df5d8aa3c50a29a
CLown1331 <clown1331@gmail.com>
2019-11-17 20:13:02 +0600

    add model

---
+++ Mongo-Crud-Console/Model/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mongo_Crud_Console.Model
+{
+    public class Book: EntityBase
+    {
+        public string BookName { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+
+        public string Author { get; set; }
+    }
+}
---
+++ Mongo-Crud-Console/Model/EntityBase.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Mongo_Crud_Console.Model
+{
+    public abstract class EntityBase
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public BsonDateTime CreateDate;
+
+        public BsonDateTime LastModifiedDate;
+    }
+}
